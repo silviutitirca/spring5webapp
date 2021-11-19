f6ccc50badccc659d78f0dcc6e546108186ff48e
silviutitirca <s_titirca@yahoo.com>
2021-11-19 17:24:57 +0100

    Adding Entities and BootStrapData

--- src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests.java
+++ src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5webapp;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +10,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Spring5webappApplicationTests {
 
+	public static String i1;
+	public static String i2;
+	public static String i3;
+	public static String i4;
+	public static String i5;
+
 	@Test
 	public void contextLoads() {
+
+		Assert.assertEquals(1,1);
+		Assert.assertEquals(1,1);
+		Assert.assertEquals(1,1);
+		Assert.assertEquals(1,1);
+		Assert.assertEquals(1,1);
 	}
 
 }
